751ce2c74f7657a0e15e34c621a49a71d069dad6
Ahmad Fariz <ahmadfariz.ae@gmail.com>
2025-02-05 22:48:36 +0700

    create a global exception filters to prepare customize application exception handling

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './exception-filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
